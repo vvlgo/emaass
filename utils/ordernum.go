7bff9f279c0dbf8e4a8244c79b2cb484c31ac9ea
vvlgo <29571935@qq.com>
2019-11-30 21:08:56 +0800

    合并审核中的代码

---
+++ utils/ordernum.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+/*
+   GetOrderNum 计算订单号，订单号格式yyyymmddDDmmss+5位随机数
+*/
+func GetOrderNum() string {
+	ntime := time.Now()
+	s := ntime.Format("20060102150405")
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	var result = ""
+	for i := 0; i < 5; i++ {
+		result += strconv.Itoa(r.Intn(10))
+	}
+	s = s + result
+	return s
+}
