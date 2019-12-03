863fcb6d4933b8bab1d39e102cadeea2e1feaf03
vvlgo <29571935@qq.com>
2019-12-03 10:27:26 +0800

    增加获取文件路径

--- utils/filetool.go
+++ utils/filetool.go
@@ -1,6 +1,9 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strings"
+)
 
 //判断文件夹是否存在
 func PathExists(path string) (bool, error) {
@@ -12,4 +15,19 @@ func PathExists(path string) (bool, error) {
 		return false, nil
 	}
 	return false, err
+}
+
+//获取绝对路径
+func getPath() string {
+	dir, _ := os.Getwd()
+
+	return dir + "/"
+}
+
+//获取上级路径
+func getParentDirectory() string {
+	dir, _ := os.Getwd()
+	index := strings.LastIndex(dir, "/")
+	runes := []rune(dir)
+	return string(runes[:index+1])
 }
